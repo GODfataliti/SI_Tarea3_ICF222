9e7e8876d089f6ae38d95d42d42bd64bbd96b6ab
Fataliti <hell.jnacho@hotmail.com>
2021-11-26 19:29:25 -0300

    creando clase regresion logistica

---
+++ cod/regresion_logistica.py
@@ -0,0 +1,10 @@
+
+#Aplicando una regresion logistica a una base de datos de datos
+
+class RegresionLogistica:
+
+    def __init__(self) -> None:
+        pass
+
+    def metodo(self):
+        pass
