0c25a668e49465a24f4dd378fac65c5ed9c7c0eb
Fataliti <hell.jnacho@hotmail.com>
2021-11-27 16:54:38 -0300

    testeo

--- cod/main.py
+++ cod/main.py
@@ -3,31 +3,35 @@ import numpy as np
 import matplotlib as plt
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
 
-
-def sktest():
-    le_geography = LabelEncoder()
-    X[:, 1] = le_geography.fit_transform(X[:, 1])
-    le_gender = LabelEncoder()
-    X[:, 2] = le_gender.fit_transform(X[:, 2])
-    ohe = OneHotEncoder(categorical_features=[1])
-    X = ohe.fit_transform(X).toarray()
-    # evitar trampa de variable irrrelevante ;)
-    X = X[:, 1:]
-    print(X)
-    print(X.shape)
-    y.shape
 
 def main():
-    
     datos = pd.read_csv('Churn_Modelling.csv')
     #print(datos.head())
     X = datos.iloc[:, 3:13].values
     y = datos.iloc[:, 13].values
+    #print(X)
+    #print(y)
+    #print(X.shape)
+
+
+    le_geography = LabelEncoder()
+    X[:, 1] = le_geography.fit_transform(X[:, 1])
+    le_gender = LabelEncoder()
+    X[:, 2] = le_gender.fit_transform(X[:, 2])
+    # evitar trampa de variable irrrelevante ;)
+    X = X[:, 1:]
     print(X)
-    print(y)
     print(X.shape)
-    sktest()
+    print(y.shape)
+
+
+
+
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
 
 
 if __name__ == '__main__':
--- cod/regresion_logistica.py
+++ cod/regresion_logistica.py
@@ -1,7 +1,17 @@
-
 #Aplicando una regresion logistica a una base de datos de datos
+# Dependencias
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
 
-class RegresionLogistica:
 
-    def __init__(self):
-        pass
+le = LabelEncoder()
+le.fit(['France', 'Tokyo', 'Germany', 'Sweden'])
+print(le.classes_)
+le.transform(['Tokyo', 'Tokyo', 'Germany', 'Sweden'])
+
+
+
+
