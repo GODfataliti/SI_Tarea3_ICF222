798a2eea02046171740e2c5b1d43eb6725d28ec9
Fataliti <hell.jnacho@hotmail.com>
2021-11-28 18:41:01 -0300

    act. cod. parte1

--- cod/regresion_logistica.py
+++ cod/regresion_logistica.py
@@ -5,18 +5,22 @@ import matplotlib as plt
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv('Churn_Modelling.csv')
 print(df.head())
 X = df.iloc[:, 3:13].values
 y = df.iloc[:, 13].values
 
+#PARTE 1: TESTEO 25% Y ENTRENAMIENTO 75%
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 print(f'x entrenamiento: {X_train}')
 print(f'x test: {X_test}')
 
 print(f'y entrenamiento: {y_train}')
 print(f'y test: {y_test}')
+
+#PARTE 2: REGRESION LOGISTICA
 
 
 
