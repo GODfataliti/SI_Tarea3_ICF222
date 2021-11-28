ee4553d70fa68fb018e2a6624f770549dac0e859
Fataliti <hell.jnacho@hotmail.com>
2021-11-28 18:24:10 -0300

    parte 1 logrado 25% test

--- cod/regresion_logistica.py
+++ cod/regresion_logistica.py
@@ -6,12 +6,17 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 
+df = pd.read_csv('Churn_Modelling.csv')
+print(df.head())
+X = df.iloc[:, 3:13].values
+y = df.iloc[:, 13].values
 
-le = LabelEncoder()
-le.fit(['France', 'Tokyo', 'Germany', 'Sweden'])
-print(le.classes_)
-le.transform(['Tokyo', 'Tokyo', 'Germany', 'Sweden'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+print(f'x entrenamiento: {X_train}')
+print(f'x test: {X_test}')
 
+print(f'y entrenamiento: {y_train}')
+print(f'y test: {y_test}')
 
 
 
