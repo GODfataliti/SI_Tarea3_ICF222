4c2f0f80b24d5bbb09b4ac12583a863fcdc58eed
Fataliti <hell.jnacho@hotmail.com>
2021-11-28 20:41:27 -0300

    parte 1: seed 1-100 agregada

--- cod/regresion_logistica.py
+++ cod/regresion_logistica.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib as plt
 import pandas as pd
+import random
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -13,7 +14,9 @@ X = df.iloc[:, 3:13].values
 y = df.iloc[:, 13].values
 
 #PARTE 1: TESTEO 25% Y ENTRENAMIENTO 75%
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+#random state numero random de la semilla entre 1 y 100
+seed = random.randint(1,100)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=seed)
 print(f'x entrenamiento: {X_train}')
 print(f'x test: {X_test}')
 
